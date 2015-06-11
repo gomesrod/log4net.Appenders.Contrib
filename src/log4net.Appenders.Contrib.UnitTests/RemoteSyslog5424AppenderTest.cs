e33d925b08c1237c4eeac786edc2c4b765eaa323
fandrei <andrei.faber@gmail.com>
2015-06-11 16:03:25 +0700

    developing a unit test

---
+++ src/log4net.Appenders.Contrib.UnitTests/RemoteSyslog5424AppenderTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using log4net.Config;
+using log4net.Layout;
+using NUnit.Framework;
+
+namespace log4net.Appenders.Contrib.UnitTests
+{
+	class RemoteSyslog5424AppenderTest
+	{
+		[Test]
+		public static void TestSimpleAppending()
+		{
+			var time = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss+00:00");
+			var id = Guid.NewGuid().ToString();
+			var message = string.Format("94 <11>1 {0} {1} {2}", time, typeof(RemoteSyslog5424AppenderTest).FullName, id);
+
+			var layout = new PatternLayout("%.255message%newline");
+			layout.ActivateOptions();
+
+			using (var appender = new RemoteSyslog5424Appender(
+				TestSettings.Server, TestSettings.Port, TestSettings.CertificatePath) { Layout = layout })
+			{
+				BasicConfigurator.Configure(appender);
+				var log = LogManager.GetLogger(typeof(RemoteSyslog5424AppenderTest));
+
+				log.Info(message);
+			}
+		}
+	}
+}
---
+++ src/log4net.Appenders.Contrib.UnitTests/TestSettings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace log4net.Appenders.Contrib.UnitTests
+{
+	static class TestSettings
+	{
+		public static string Server
+		{
+			get { return Get("Log4netContribTests_Server"); }
+		}
+
+		public static int Port
+		{
+			get { return int.Parse(Get("Log4netContribTests_Port")); }
+		}
+
+		public static string CertificatePath
+		{
+			get { return Get("Log4netContribTests_CertificatePath"); }
+		}
+
+		static string Get(string name)
+		{
+			var res = Environment.GetEnvironmentVariable(name);
+			if (string.IsNullOrEmpty(res))
+				throw new ApplicationException(string.Format("Environment variable {0} is not found", name));
+			return res;
+		}
+	}
+}
