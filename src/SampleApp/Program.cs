2cf0d5d917ac0ce03fffc3f418bdb168926de1c0
fandrei <andrei.faber@gmail.com>
2015-06-12 15:25:13 +0700

    sample application added

---
+++ src/SampleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using log4net.Config;
+
+namespace log4net.Appenders.Contrib.SampleApp
+{
+	static class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				XmlConfigurator.Configure();
+
+				var message = Guid.NewGuid().ToString();
+				var log = LogManager.GetLogger(typeof (Program));
+				log.Info(message);
+			}
+			catch (Exception exc)
+			{
+				Console.WriteLine(exc);
+				if (Debugger.IsAttached)
+					Debugger.Break();
+			}
+		}
+	}
+}
