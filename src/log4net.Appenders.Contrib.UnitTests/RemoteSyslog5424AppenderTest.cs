78f9f154ce324587cdfb9b47a247a14bb5c2ba0f
Andrei Faber <andrei.faber@gmail.com>
2015-09-15 12:18:02 +0700

    code cleanup

--- src/log4net.Appenders.Contrib.UnitTests/RemoteSyslog5424AppenderTest.cs
+++ src/log4net.Appenders.Contrib.UnitTests/RemoteSyslog5424AppenderTest.cs
@@ -11,27 +11,5 @@ namespace log4net.Appenders.Contrib.UnitTests
 {
 	class RemoteSyslog5424AppenderTest
 	{
-		[Test]
-		public static void TestSimpleAppending()
-		{
-			var layout = new PatternLayout("%.255message");
-			layout.ActivateOptions();
-
-			using (var appender = new RemoteSyslog5424Appender(TestSettings.Server, TestSettings.Port, TestSettings.CertificatePath))
-			{
-				appender.Layout = layout;
-				appender.AppName = typeof(RemoteSyslog5424AppenderTest).Name;
-				appender.ActivateOptions();
-
-				BasicConfigurator.Configure(appender);
-				var log = LogManager.GetLogger(typeof(RemoteSyslog5424AppenderTest));
-
-				for (var i = 0; i < 3; i++)
-				{
-					var message = i + "_" + Guid.NewGuid();
-					log.Info(message);
-				}
-			}
-		}
 	}
 }
